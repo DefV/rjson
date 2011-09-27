d8ebe9d5e72eed0398bb1c61f343ba293f0e483a
Jan De Poorter <jan@defv.be>
2011-09-27 15:58:47 +0200

    Don't inherit from deprecated class or include deprecated module

--- lib/rjson/handler.rb
+++ lib/rjson/handler.rb
@@ -2,19 +2,20 @@ require 'action_view/template'
 require 'rjson/string'
 
 module RJSON
-  class Handler < ActionView::TemplateHandler
-    include ActionView::Template::Handlers::Compilable
-    
+  class Handler
+    class_attribute :default_format
     self.default_format = Mime::JSON
     
-    def compile(template)
-      <<-COMPILED_METHOD
-        contents=begin;
-          #{template.source}
-        end
+    class << self
+      def call(template)
+        <<-COMPILED_METHOD
+          contents=begin;
+            #{template.source}
+          end
         
-        RJSON::String.new(contents.to_json)
-      COMPILED_METHOD
+          RJSON::String.new(contents.to_json)
+        COMPILED_METHOD
+      end
     end
   end
 end
